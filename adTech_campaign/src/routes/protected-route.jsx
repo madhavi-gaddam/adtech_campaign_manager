e8e7d1e3c12b98b228b76b7b67aff76ba1debd89
madhavi-gaddam <madhavi.gaddam@fissionlabs.com>
2026-07-15 11:08:01 +0530

    updated folder structure and app routes

--- adTech_campaign/src/routes/protected-route.jsx
+++ adTech_campaign/src/routes/protected-route.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import { Navigate, useLocation } from "react-router-dom";
-import { AuthContext } from "../../context/AuthContextValue";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { AuthContext } from "../context/AuthContextValue";
 
-export function RequireAuth({ children, roles }) {
+export function ProtectedRoute({ roles }) {
   const { currentUser } = useContext(AuthContext);
   const location = useLocation();
 
   if (!currentUser) return <Navigate to="/signup" replace state={{ from: location }} />;
   if (roles && !roles.includes(currentUser.role)) return <Navigate to="/" replace />;
-  return children;
+  return <Outlet />;
 }
